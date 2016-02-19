04e6ca3798c4ddb5e55a63dca1c1b2978756a432
pulkitpahwa <pulkitpahwa11@gmail.com>
2016-02-19 13:54:29 +0530

    contest question pool template modified. settings and urls update for static files

--- mcq/mcq/urls.py
+++ mcq/mcq/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+import settings
 
 urlpatterns = [
     url(r'^$', 'mcq.views.home', name='home'),
     url(r'^mcq/', include('Questions.urls')),
     url(r'^contest/', include('contest.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^site_media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
 ]
